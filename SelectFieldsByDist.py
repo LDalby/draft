f0b72dafec0170759bdca106e7b47629ef01136a
LDalby <lars@bios.au.dk>
2016-04-28 15:50:46 +0200

    almost there

--- SelectFieldsByDist.py
+++ SelectFieldsByDist.py
@@ -1,6 +1,6 @@
 # Read ascii of ALMaSS landscape 
 # Remap values to goose numbers from sim
-print('foo')
+
 # Import system modules
 from arcpy import env
 import arcpy, traceback, sys, time, gc, os
@@ -18,70 +18,44 @@ arcpy.MakeFeatureLayer_management(inFields, "inFields_lyr")
 
 locations = ['KostraedeBanker', 'RavnstrupSoe', 'TingvadBorum', 'AuKaloe', 'HjortkaerEndrup', 'HHGlumsoe']
 pathtofields = "O:/ST_Starlings/GIS/Fields/"
+# Import the ringing sites
+x_coords = "X"
+y_coords = "Y"
+out_Layer = "ringingsites"
+saved_shp = "C:/Users/lada/Desktop/ringingsites.shp"
+if arcpy.Exists(saved_shp):
+        arcpy.Delete_management(saved_shp)
+# Make the XY event layer...
+arcpy.MakeXYEventLayer_management(inRingingSites, x_coords, y_coords, out_Layer, prj)
+# Copy layer to generate OIDs:
+arcpy.CopyFeatures_management(out_Layer, saved_shp)
+# Convert shape to feature:
+arcpy.MakeFeatureLayer_management(saved_shp, "RingSite")
+# Define function to construct WHERE clause:
+def buildWhereClause(table, field, value):
+  """Constructs a SQL WHERE clause to select rows having the specified value
+  within a given field and table."""
+  # Add DBMS-specific field delimiters
+  fieldDelimited = arcpy.AddFieldDelimiters(table, field)
+  # Determine field type
+  fieldType = arcpy.ListFields(table, field)[0].type
+  # Add single-quotes for string field values
+  if str(fieldType) == 'String':
+      value = "'%s'" % value
+  # Format WHERE clause
+  whereClause = "%s = %s" % (fieldDelimited, value)
+  return whereClause
 
-
-try:
-	# Import the ringing sites
-	# Set the local variables
-	x_coords = "X"
-	y_coords = "Y"
-	out_Layer = "ringingsites"
-	# saved_Layer = "C:/Users/lada/Desktop/ringingsites.lyr"
-	saved_shp = "C:/Users/lada/Desktop/ringingsites.shp"
-	selected_ringsite = "C:/Users/lada/Desktop/kaloe.shp"
-	# Make the XY event layer...
-	arcpy.MakeXYEventLayer_management(inRingingSites, x_coords, y_coords, out_Layer, prj)
-	# Copy layer to generate OIDs:
-	arcpy.CopyFeatures_management(out_Layer, saved_shp)
-	# Convert shape to feature:
-	arcpy.MakeFeatureLayer_management(saved_shp, "RingSite")
+for index in range(len(locations)):
+	fieldname = "Location"
+	fieldvalue = locations[index]
+	where_clause = buildWhereClause("RingSite", fieldname, fieldvalue)
 	# Select one ringing site:
-	arcpy.SelectLayerByAttribute_management("RingSite", "NEW_SELECTION", "\"Location\" = 'AuKaloe'")
-	# # Select by distance
+	arcpy.SelectLayerByAttribute_management("RingSite", "NEW_SELECTION", where_clause)
+	# Select by distance
 	arcpy.SelectLayerByLocation_management("inFields_lyr",'WITHIN_A_DISTANCE', "RingSite", "2000 Meters", 'NEW_SELECTION', "NOT_INVERT")
-	# arcpy.DefineProjection_management(outRaster, prj)
-	arcpy.CopyFeatures_management('inFields_lyr', 'C:/Users/lada/Desktop/AuKaloeFields.shp')
+	# Make the file name
+	fieldlocfile = locations[index] + ".shp"
+	fieldloc = os.path.join(pathtofields, fieldlocfile)
+	arcpy.CopyFeatures_management('inFields_lyr', fieldloc)
 
-
-except:
-	tb = sys.exc_info()[2]
-	tbinfo = traceback.format_tb(tb)[0]
-	pymsg = "PYTHON ERRORS:\nTraceback Info:\n" + tbinfo + "\nError Info:\n     " +        str(sys.exc_type) + ": " + str(sys.exc_value) + "\n"
-	msgs = "ARCPY ERRORS:\n" + arcpy.GetMessages(2) + "\n"
-
-	arcpy.AddError(msgs)
-	arcpy.AddError(pymsg)
-
-	print msgs
-	print pymsg
-
-	arcpy.AddMessage(arcpy.GetMessages(1))
-	print arcpy.GetMessages(1)
-
-
-
-# Tjek det shapefile.strip trick ud:
-# Name: CopyFeatures_Example2.py
-# Description: Convert all shapefiles in a folder to geodatabase feature classes
-# Requirements: os module
- 
-# Import system modules
-import arcpy
-from arcpy import env
-import os
- 
-# Set environment settings
-env.workspace = "C:/data"
- 
-# Set local variables
-outWorkspace = "c:/output/output.gdb"
- 
-# Use ListFeatureClasses to generate a list of shapefiles in the
-#  workspace shown above.
-fcList = arcpy.ListFeatureClasses()
- 
-# Execute CopyFeatures for each input shapefile
-for shapefile in fcList:
-    # Determine the new output feature class path and name
-    outFeatureClass = os.path.join(outWorkspace, shapefile.strip(".shp"))
-    arcpy.CopyFeatures_management(shapefile, outFeatureClass)
